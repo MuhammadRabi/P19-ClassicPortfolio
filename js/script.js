5393b03527c2cde57b5aac3e7f29837198d9265d
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2021-01-21 21:34:57 +0200

    markup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // closing side menu
 const menuToggle = document.querySelector(".menu-toggle");
+const sideBar = document.querySelector(".sidebar");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
+  sideBar.classList.toggle("active");
 });
