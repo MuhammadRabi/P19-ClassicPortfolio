4f136d8f2d4e114a87f7c295c39d20fe63d4198b
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2021-01-22 02:24:41 +0200

    logic
    scroll to top + scroll to sec

--- js/script.js
+++ js/script.js
@@ -6,3 +6,49 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   sideBar.classList.toggle("active");
 });
+
+// scroll to top button
+const scrollToTop = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", showScrollBtn);
+
+function showScrollBtn() {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 200) {
+    scrollToTop.classList.add("show-scroll");
+  } else {
+    scrollToTop.classList.remove("show-scroll");
+  }
+}
+
+scrollToTop.addEventListener("click", goUp);
+
+function goUp() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+// smooth scroll to section
+const links = document.querySelectorAll(".sidebar ul li a");
+
+function scrollToSection(element) {
+  element.forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      // remove active classes
+      element.forEach((activeLink) => {
+        activeLink.classList.remove("active");
+      });
+      anchor.classList.add("active");
+      const section = document.querySelector(e.target.dataset.link).offsetTop;
+      window.scrollTo({
+        top: section,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+scrollToSection(links);
